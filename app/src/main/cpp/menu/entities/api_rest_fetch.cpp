c237e118af5e87463cb797c359a23fb018622e09
tseOrlando <20sw@GMAIL.COM>
2024-05-09 16:43:38 +0200

    adapted generate_request function to be able to make post requests with data

--- app/src/main/cpp/menu/entities/api_rest_fetch.cpp
+++ app/src/main/cpp/menu/entities/api_rest_fetch.cpp
@@ -13,7 +13,6 @@ size_t api_rest_fetch::write_callback(void *contents, size_t size, size_t nmemb,
     return total_size;
 }
 
-
 std::string api_rest_fetch::generate_request(const std::string &url, const std::string &request_type, const std::string& post_data) noexcept
 {
     CURL *curl = curl_easy_init();
@@ -27,6 +26,9 @@ std::string api_rest_fetch::generate_request(const std::string &url, const std::
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_buffer);
 
+        if (request_type == "POST")
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_data.c_str());
+
         res = curl_easy_perform(curl);
 
         if (res != CURLE_OK)
@@ -38,17 +40,8 @@ std::string api_rest_fetch::generate_request(const std::string &url, const std::
     return response_buffer;
 }
 
-std::string api_rest_fetch::_get(const std::string& url) noexcept
-{
-    return generate_request(url, "GET");
-}
+std::string api_rest_fetch::_get(const std::string& url) noexcept { return generate_request(url, "GET"); }
 
-std::string api_rest_fetch::_post(const std::string& url, const std::string& data) noexcept
-{
-    return generate_request(url, "POST");
-}
+std::string api_rest_fetch::_post(const std::string& url, const std::string& data) noexcept { return generate_request(url, "POST", data); }
 
-std::string api_rest_fetch::_delete(const std::string& url) noexcept
-{
-    return generate_request(url, "DELETE");
-}
+std::string api_rest_fetch::_delete(const std::string& url) noexcept { return generate_request(url, "DELETE"); }
